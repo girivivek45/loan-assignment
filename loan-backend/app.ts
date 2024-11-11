c4b476ec925da14b3f12d62b1cabb13686021867
girivivek45 <vivek.21ug2020@iiitranchi.ac.in>
2024-11-12 00:00:18 +0530

    added updated backend url

--- loan-backend/app.ts
+++ loan-backend/app.ts
@@ -20,7 +20,7 @@ const connectDB = async () => {
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://creditsea-rust.vercel.app/",
     credentials:true
 }))
 
