c457393acf084f39bed02c3cf092651a1908ba31
girivivek45 <vivek.21ug2020@iiitranchi.ac.in>
2024-11-20 22:40:59 +0530

    check12

--- loan-backend/routes/role.ts
+++ loan-backend/routes/role.ts
@@ -2,5 +2,5 @@ import { Request } from "express"
 import { IUser } from "../models/user"
 export interface IGetUser extends Request {
     user?:IUser;
-  role?: string // or any other type
+  role?: string 
 }
